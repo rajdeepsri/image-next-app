444e25389e64e0780cd74be2d4d191115db8fb2d
Rajdeep Shrivastava <rajdeep.shrivastava@stackbox.xyz>
2024-11-09 17:37:32 +0530

    initial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
+
+export const metadata: Metadata = {
+  title: 'SnapNest',
+  description: 'Image Gallery Application',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="antialiased">
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          <nav className="border border-white h-20"></nav>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
