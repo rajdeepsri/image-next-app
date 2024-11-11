a11ecac54d34b355ac9efc60792751787f946e6e
rajdeepsri <rajdeep1999.srivastav@gmail.com>
2024-11-11 22:29:12 +0530

    added useMediaQuery hook

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,8 +2,11 @@
 import { FC, useState } from 'react'
 import { Button } from './ui/button'
 import { useRouter } from 'next/navigation'
+import { Search } from 'lucide-react'
+import { useMediaQuery } from '@/lib/hooks'
 
 export const SearchBar: FC = () => {
+  const isMobile = useMediaQuery('(max-width: 640px)')
   const router = useRouter()
   const [searchQuery, setSearchQuery] = useState<string>('')
 
@@ -21,17 +24,35 @@ export const SearchBar: FC = () => {
 
   return (
     <>
-      <input
-        type="text"
-        className="bg-transparent outline-none border-none text-neutral-800 font-medium w-full text-xs sm:text-base"
-        placeholder="Search high resolution Images, categories, wallpapers"
-        value={searchQuery}
-        onChange={e => setSearchQuery(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
-      <Button variant="outline" className="h-10 my-3 px-8" onClick={goToSearchPage}>
-        Search
-      </Button>
+      <div className="flex gap-1 sm:gap-2 px-2 sm:px-4 py-1 sm:py-0 mt-1 rounded-sm sm:rounded-md items-center bg-neutral-50 w-[90%] sm:w-[60%]">
+        <Search color="#696969" />
+        <input
+          type="text"
+          className="bg-transparent outline-none border-none text-neutral-800 font-medium w-full text-xs sm:text-base"
+          placeholder="Search high resolution Images, categories, wallpapers"
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        {!isMobile && (
+          <Button
+            variant="outline"
+            className="h-10 my-3 text-sm px-8 dark:text-neutral-200 text-neutral-950"
+            onClick={goToSearchPage}
+          >
+            Search
+          </Button>
+        )}
+      </div>
+      {isMobile && (
+        <Button
+          variant="outline"
+          className="h-8 my-3 px-6 text-xs dark:text-neutral-200 text-neutral-950"
+          onClick={goToSearchPage}
+        >
+          Search
+        </Button>
+      )}
     </>
   )
 }
